4ca0bab3294ca6ddcf06736166b2d0ab91dc79b4
Krzysztof Huczek <krzysztof.huczek@gmail.com>
2021-05-23 14:58:53 +0200

    project version with flask

--- src/static_web_folder/js/utils.js
+++
@@ -1,9 +0,0 @@
-function changeNavColor(id) {
-  document.getElementById(id).style.color = "white";
-}
-
-function redirect(path) {
-  window.location.replace(path);
-}
-
-export default { redirect, changeNavColor };
